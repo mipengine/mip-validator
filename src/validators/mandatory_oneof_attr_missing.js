7217d41d22261f248c18145942ca1aacf8ec3e84
harttle <yangjvn@126.com>
2016-08-22 22:35:14 +0800

    match condition for MANDATORY_ONEOF_ATTR_MISSING, fix #6

--- src/validators/mandatory_oneof_attr_missing.js
+++ src/validators/mandatory_oneof_attr_missing.js
@@ -1,14 +1,20 @@
 const _ = require('lodash');
 const ERR = require('../error.json');
+const matcher = require('../matcher.js');
+const util = require('util');
 
 exports.onNode = function(node, nodeRule, engine) {
     var attrOccurrence = _.keyBy(node.attrs, 'name');
 
     _.forOwn(nodeRule.attrs, (rules, attrName) => {
         rules.map(rule => {
-            if (rule.mandatory && !attrOccurrence[attrName]) {
+            if (rule.mandatory &&
+                matcher.matchAttrs(node, rule.match) &&
+                !attrOccurrence[attrName]) {
+
                 var err = ERR.MANDATORY_ONEOF_ATTR_MISSING;
-                engine.createError(err.code, err.message, node.__location);
+                var msg = util.format(err.message, node.nodeName, attrName);
+                engine.createError(err.code, msg, node.__location);
             }
         });
     });
--- test/mandatory_oneof_attr_missing.js
+++ test/mandatory_oneof_attr_missing.js
@@ -13,6 +13,16 @@ describe('mandatory oneof attr missing', function() {
                         mandatory: true
                     }
                 }
+            },
+            span: {
+                attrs: {
+                    id: {
+                        mandatory: true,
+                        match: {
+                            style: 'none'
+                        }
+                    }
+                }
             }
         });
     });
@@ -23,7 +33,9 @@ describe('mandatory oneof attr missing', function() {
     it('should reject invalid mandatory attr', function() {
         var result = validator.validate('<div></div>');
         expect(result).to.have.lengthOf(1);
-        expect(result[0].code).to.equal(errorCode.MANDATORY_ONEOF_ATTR_MISSING.code);
+        var err = errorCode.MANDATORY_ONEOF_ATTR_MISSING;
+        expect(result[0].code).to.equal(err.code);
+        expect(result[0].message).to.equal("标签'div'的强制性属性'id'缺失");
     });
     it('should accept when no tag presence', function() {
         var result = validator.validate('<p></p>');
@@ -32,6 +44,11 @@ describe('mandatory oneof attr missing', function() {
     it('should accept when no tag presence', function() {
         var result = validator.validate('<p></p>');
         expect(result).to.have.lengthOf(0);
-//标签'xx'的强制性属性'xx'缺失
+    });
+    it('should respect match', function() {
+        var result = validator.validate('<span></span>');
+        expect(result).to.have.lengthOf(0);
+        result = validator.validate('<span style="none"></span>');
+        expect(result).to.have.lengthOf(1);
     });
 });
