0c4ca7e28585a2d3a92efcb0af2e5fa6a1d09f1d
harttle <yangjvn@126.com>
2017-03-21 13:35:40 +0800

    update error table generator

--- bin/md-error.js
+++ bin/md-error.js
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
+
 var ERR = require('../src/error.json');
 var _ = require('lodash');
 
-console.log('错误代码 | 错误消息 | 备注')
-console.log('---      | ---      | ---')
+console.log('错误代码 | 错误标识 | 错误消息');
+console.log('---      | ---      | ---');
 
 _.map(ERR, (v, k) => {
-    console.log(`${v.code} | ${v.message} | ${v.misc || '无'}`);
+    console.log(`${v.code} | \`${k}\` | ${v.message}`);
 });
 
+
+console.log('\n>该文件由 node bin/md-error.js 自动生成，请勿直接更改本文件。');
