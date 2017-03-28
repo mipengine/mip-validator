67def9751946318a7f5bb2759eaa99331995f877
harttle <yangjvn@126.com>
2017-03-28 10:53:29 +0800

    modified:   src/validators/invalid_inner_html.js

--- src/validators/invalid_inner_html.js
+++ src/validators/invalid_inner_html.js
@@ -6,7 +6,7 @@ exports.onNode = function(node, rule, error, engine) {
     if (rule.inner_html === undefined) return;
 
     var loc = node.__location;
-    var innerHTML = engine.html.slice(loc.startTag.endOffset, loc.endTag.startOffset).replace(/(\s*)/g, "");
+    var innerHTML = engine.html.slice(loc.startTag.endOffset, loc.endTag.startOffset);
     if(matcher.matchValue(innerHTML, rule.inner_html)) return;
 
     var err = ERR.INVALID_INNER_HTML;
