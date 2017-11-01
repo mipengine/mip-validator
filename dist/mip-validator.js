d8c7f5d1bd86d11f5e9bf1476d66e231b1308866
Jackson <wupeng_xg@163.com>
2017-11-01 16:13:10 +0800

    feat:head中引入js必须使用async (#14)
    * feat:head中引入js必须使用async
    
    * add async case
    
    * update case readme
    
    * interge script rules
    
    * interge script rules
    
    * interge script rules

--- dist/mip-validator.js
+++ dist/mip-validator.js
@@ -2941,7 +2941,7 @@ if (typeof Object.create === 'function') {
 /*!
  * Determine if an object is a Buffer
  *
- * @author   Feross Aboukhadijeh <feross@feross.org> <http://feross.org>
+ * @author   Feross Aboukhadijeh <https://feross.org>
  * @license  MIT
  */
 
@@ -16755,7 +16755,7 @@ function hasOwnProperty(obj, prop) {
 },{"./support/isBuffer":56,"_process":36,"inherits":55}],58:[function(require,module,exports){
 module.exports={
     "name": "mip-validator",
-    "version": "1.5.9",
+    "version": "1.5.10",
     "description": "MIP validator",
     "main": "index.js",
     "dependencies": {
@@ -16810,6 +16810,9 @@ module.exports={
         "sinon-chai": "^2.8.0",
         "supertest": "^2.0.1",
         "uglifyjs": "^2.4.10"
+    },
+    "engines": {
+        "node": ">=4.0.0"
     },
     "scripts": {
         "test": "DEBUG='mip-validator:' mocha",
@@ -16940,10 +16943,31 @@ module.exports={
 
         }
     },
-    "script": [{
+    "script": [
+    {
         "mandatory": {
-             "type": "/^(text\/javascript)?/",
-             "src": "/^(https:)?\/\/(mipcache.bdstatic.com\/static\/mipmain)|(m.baidu.com\/static\/ala\/sf\/static\/)|((https:)?\/\/mipcache.bdstatic.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/mipmain)/"
+            "type": "/^(text\/javascript)?/",
+            "src": "/^(https:)?\/\/(mipcache.bdstatic.com\/static\/mipmain)|(m.baidu.com\/static\/ala\/sf\/static\/)|((https:)?\/\/mipcache.bdstatic.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/mipmain)/"
+        }
+    },
+    {
+        "match": {
+            "src": "/^(https:)?\/\/(mipcache.bdstatic.com\/static\/mipmain)|(m.baidu.com\/static\/ala\/sf\/static\/)|((https:)?\/\/mipcache.bdstatic.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/mipmain)/"
+        },
+        "attrs": {
+            "type": {
+                "value": "/^(text\/javascript)$/"
+            }
+        }
+    }, {
+        "match_ancestor": "head",
+        "match": {
+            "src": "/^(https:)?\/\/(mipcache.bdstatic.com\/static\/mipmain)|(m.baidu.com\/static\/ala\/sf\/static\/)|((https:)?\/\/mipcache.bdstatic.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/v\\d\/)|((https:)?\/\/c.mipcdn.com\/static\/mipmain)/"
+        },
+        "attrs": {
+            "async": {
+                "mandatory": true
+            }
         }
     }, {
         "disallow": true,
@@ -16953,15 +16977,6 @@ module.exports={
         }
     }, {
         "disallowed_ancestor": "template"
-    },{
-         "attrs": {
-            "async": [{
-                "match": {
-                    "match_ancestor": "head"
-                },
-                "mandatory": true
-            }]
-        }
     }],
     "mip-input": {
         "mandatory_ancestor": "mip-form",
