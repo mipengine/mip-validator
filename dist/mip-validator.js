54d4d9b764ac31583d428c13fc550efc3d74f4fd
wupengFEX <smartfutureplayer@gmail.com>
2018-01-22 18:40:05 +0800

    add cases

--- dist/mip-validator.js
+++ dist/mip-validator.js
@@ -17031,7 +17031,7 @@ module.exports={
     },
     {
         "match": {
-            "src": "/^(https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))(\/[.|\n]*)?$/"
+            "src": "/^(https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))(\/([\\s\\S]*))?$/"
         },
         "attrs": {
             "type": {
@@ -17041,7 +17041,7 @@ module.exports={
     }, {
         "match_ancestor": "head",
         "match": {
-            "src":"/^(https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))(\/[.|\n]*)?$/"        
+            "src": "/^(https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))(\/([\\s\\S]*))?$/"
         },
         "attrs": {
             "async": {
@@ -17052,7 +17052,7 @@ module.exports={
         "disallow": true,
         "match": {
             "type": "/^(?!application\/(ld\\+)?json$)/",
-            "src":"/^(?!((https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))))/"
+            "src":"/^(?!((https:)?\/\/((mipcache\\.bdstatic\\.com)|(c\\.mipcdn\\.com))(\/([\\s\\S]*))?$))/"
         }
     }, {
         "disallowed_ancestor": "template"
