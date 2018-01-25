0325b0ce41ddd98ef939ba3af11576f79d9d8c96
Jackson <wupeng_xg@163.com>
2018-01-25 19:53:51 +0800

    feat: 修改 link 校验规则 (#20)
    * feat: 修改 link 校验规则
    
    * docs: 修改大小写
    
    * 修改mip-form正则
    
    * fix: 修改utf-8判断问题
    
    * 增加 source 父元素类型
    
    * 增加 initical-scale 校验
    
    * 修改测试 cases
    
    * 修改测试 cases
    
    * 修改测试 cases
    
    * 修改js正则
    
    * 加入属性选择写法
    
    * 修改脚本正则
    
    * 修改脚本正则
    
    * 修改脚本正则
    
    * 修改cases
    
    * 增加cases && 修改不标准规则
    
    * 修改 link 规则
    
    * 增加 cases
    
    * 增加测试 cases
    
    * 修改script和link规则
    
    * 修改script和link cases
    
    * 单测增加cases测试
    
    * 修复bug
    
    * 修改script规则
    
    * 修改规则
    
    * add cases
    
    * add cases
    
    * 更新版本号

---
+++ test/mandatory_attr_or_missing.js
@@ -0,0 +1,30 @@
+const chai = require('./chai.js')
+const expect = chai.expect
+const Validator = require('..')
+const ERR = require('../src/error/dfn.json')
+
+describe('mandatory attributes missing', function() {
+    var result
+    it('valid mandatory attributes or', function() {
+        var validator = Validator({
+            img: {
+                "attrs_or": [{
+                    "src": "/^\\S+$/"
+                }, {
+                    "srcset": "/^\\S+$/"
+                }],
+            }
+        })
+        result = validator.validate('<img>')    
+        expect(result).to.have.lengthOf(1)
+    })
+    
+    it('invalid mandatory attributes or', function() {
+        var validator = Validator({
+            img: {
+            }
+        })
+        result = validator.validate('<img>')    
+        expect(result).to.have.lengthOf(0)
+    })
+})
